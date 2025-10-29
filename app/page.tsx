d3cf4ff761c2951c580d99dcd00e9387aa8f5036
NOVASWAY <njauwangari6@gmail.com>
2025-10-29 08:08:33 +0300

    Add highly visible deployment test changes to verify if GitHub Pages is updating

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         <Services />
       </main>
       <Footer />
-      {/* Deployment test - updated */}
+      {/* DEPLOYMENT TEST - UPDATED AT $(date) */}
     </div>
   )
 }
